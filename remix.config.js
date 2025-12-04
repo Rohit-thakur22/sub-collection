38b03248190d044fab26d1c92c3f828089e38cbb
abhishek004444 <abhishekjain00444@gmail.com>
2025-12-05 02:47:31 +0530

    change in code

--- remix.config.js
+++ remix.config.js
@@ -15,6 +15,9 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   serverModuleFormat: "cjs",
-  dev: { port: process.env.HMR_SERVER_PORT || 8002 },
+  dev: { 
+    port: process.env.HMR_SERVER_PORT || 8002 ,
+    host: "127.0.0.1"
+  },
   future: {},
 };
